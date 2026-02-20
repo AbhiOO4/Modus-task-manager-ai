464f2632adfc021dc2d9f39fa12ff90703df1bfa
ABHINAV SREEJITH <145277674+AbhiOO4@users.noreply.github.com>
2026-02-20 14:46:10 +0530

    Update axios baseURL to include '/api' path

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://modus-task-manager.onrender.com",
+  baseURL: "https://modus-task-manager.onrender.com/api",
   withCredentials: true,   
 });
 
