0da43fcad36a9648b7be07ad1dc20bd299a851df
Abhinav Sreejith <abhinavsree243@gmail.com>
2026-02-20 21:07:31 +0530

    bug fix

--- backend/src/utils/generateTokenAndSetCookie.js
+++ backend/src/utils/generateTokenAndSetCookie.js
@@ -2,11 +2,12 @@ import jwt from 'jsonwebtoken'
 
 export const generateTokenAndSetCookie = (res, userId) => {
     const token = jwt.sign({ userId }, process.env.JWT_SECRET, {expiresIn: '7d'})
+
     
     res.cookie('token', token, {
         httpOnly: true, //cant be accesed with js avoid xss attacks
         secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict', //csrf
+        sameSite: 'none', //csrf
         maxAge: 7*24*60*60*1000
     });
 
